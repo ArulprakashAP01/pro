8f0b923781143a25d4fd576c8c114892a61d77cf
Arul <arulprakashr02@gmail.com>
2025-07-16 12:38:50 +0530

    Create comment.php

---
+++ comment.php
@@ -0,0 +1,26 @@
+<?php
+if (!isset($_COOKIE['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $comment = $_POST['comment'];
+    file_put_contents("comments.txt", $comment . "\n", FILE_APPEND);
+}
+
+$comments = file("comments.txt");
+?>
+
+<h2>Welcome, <?php echo $_COOKIE['username']; ?>!</h2>
+<form method="POST">
+    <textarea name="comment" placeholder="Write a comment"></textarea><br>
+    <button type="submit">Post Comment</button>
+</form>
+
+<h3>Previous Comments:</h3>
+<?php
+foreach ($comments as $c) {
+    echo "<p>$c</p>";
+}
+?>
