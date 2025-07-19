40861f8280fbbd7ff27e00c2d00c01d560573a0c
Arul <arulprakashr02@gmail.com>
2025-07-19 17:43:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 <?php
 sdhhhjjhghjgjdsdhhhghhsdkhsdhghgfsdskhhhfh
-shhghgjhhhghhhhgdgsdkhsd
+shhghgjhhhghhhhgdgsdkhsdhghgh
 if (isset($_GET['user'])) {
     $conn = mysqli_connect("localhost", "root", "", "test");
     $user = $_GET['user'];
