c5af040ced1fec93a63a5c13344c362213b49f42
ArulprakashAP01 <arulprakashr02@gmail.com>
2025-07-19 17:52:18 +0530

    ndds

--- comment.php
+++ comment.php
@@ -3,7 +3,7 @@ if (!isset($_COOKIE['username'])) {
     header("Location: login.php");;
     exit();
 }
-
+sdhd
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $comment = $_POST['comment'];
     file_put_contents("comments.txt", $comment . "\n", FILE_APPEND);
